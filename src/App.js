29be8ac5cd25b736d017a307dc618122794a3bad
Dan <danielzuzevich@gmail.com>
2022-04-19 10:55:24 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [files, setFiles] = useState([]);
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
+  const handleDragDrop = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const handleFiles = (fileList) => {
+    setFiles((prevFiles) => [...prevFiles, ...Array.from(fileList)]);
+  };
+
+  const removeFile = (name) => {
+    const newFiles = files.filter((file) => file.name !== name);
+    setFiles(newFiles);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="inner">
+        <div className="list">
+          <h5>Your files:</h5>
+
+          <ul>
+            {files.map((file, i) => (
+              <li key={file.name}>
+                {i + 1}. {file.name}
+                <span onClick={() => removeFile(file.name)}>
+                  <i className="fa fa-times" />
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div
+          className="form"
+          onDragEnter={handleDragDrop}
+          onDragOver={handleDragDrop}
+          onDrop={(e) => {
+            handleDragDrop(e);
+            handleFiles(e.dataTransfer.files);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <i className="fa fa-cloud-upload fa-4x"></i>
+          <p>Drag and drop files or select files below.</p>
+
+          <input
+            ref={inputRef}
+            type="file"
+            multiple
+            style={{ display: 'none' }}
+            onChange={(e) => {
+              e.preventDefault();
+              handleFiles(e.target.files);
+            }}
+          />
+
+          <br />
+
+          <button onClick={handleClick}>Choose Files</button>
+        </div>
+      </div>
     </div>
   );
 }
